30d469c34aece47c6fab7bcb95b752b709902aa9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-06-01 13:00:00 +0900

    chore(deps): Elixir 요구사항 및 개발 의존성 업그레이드
    - Elixir 버전을 "~> 1.17"에서 "~> 1.19"로 상향
    - ex_doc을 "~> 0.34"에서 "0.40.3"으로 업데이트 (mix.exs, mix.lock)
    - mix.lock에서 credo, earmark_parser, file_system, jason, makeup_erlang 등 관련 패키지 버전 업데이트

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LeetcodeElixir.MixProject do
     [
       app: :leetcode_elixir,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,9 +22,7 @@ defmodule LeetcodeElixir.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.40.3", only: :dev, runtime: false}
     ]
   end
 end
